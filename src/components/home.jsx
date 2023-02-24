f8c6d309c8550daca649c8c19106d8862d9e7402
vermashiva12 <shivanshuverma687@gmail.com>
2023-02-24 15:16:46 -0500

    updated

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,8 @@ import ME from '../assets/me.jpg'
 const Home = () => {
   return (
     <div name = 'home' className='w-full h-screen bg-[#31335b]'>
-      
+
+      <div className='py-12'></div>
       <div  className=' mx-auto px-2 flex flex-col justify-center   text-gray-100 text-[20px] h-full'>
         {/* Container*/}
 
@@ -14,11 +15,8 @@ const Home = () => {
           <p className='font-bold text-center justify-center text-[40px] py-2 '>Welcome to My Portfolio !!</p>
         </div>
         
-        <div className='w-full grid grid-cols-2 sm:grid-cols-1 gap-4 text-center py-6'>
-                <div className='shadow-lg hover:scale-110 duration-500 '>
-                    <img src={ME} alt="ME" className='w-20 mx-auto ' style={{width: '350px', height:'420px'}}/>
-                   
-                </div>
+        <div className='w-full grid md:grid-cols-2 sm:grid-cols-1 gap-4 text-center py-6'>
+              
         <div className='py-16'>
             <p>Hi, My Name is </p>
             <h1 className='text-4xl sm:text-7xl font-bold'> Shivanshu Verma</h1>
@@ -27,6 +25,10 @@ const Home = () => {
            
         
          </div>
+         <div className='shadow-md hover:scale-110 duration-500 '>
+                    <img src={ME} alt="ME" className='w-20 mx-auto ' style={{width: '350px', height:'420px'}}/>
+                   
+                </div>
       </div>
       <div className=' mx-auto justify-center'>
                 <button className='border-2 group px-5 py-3 my-2 flex items-center hover: bg-black hover:border-white'>
